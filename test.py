0375fbbaea747fa5c0f03287efa0820be1b3dd90
Naty-J <natalie.jaresova@student.gyarab.cz>
2024-11-21 11:06:18 +0100

    fibonacci corection

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 def fibonacci(n):
-    if not isinstance(n, int) or n <= 0:
-        raise ValueError("n must be a positive integer")
+    if not isinstance(n, int) or n < 0:
+        raise ValueError("n must be a non-negative integer")
+    
+    if n == 0:
+        return 0
     
     sequence = [0, 1]  # Initialize the sequence with the first two numbers
     while len(sequence) < n:
         next_number = sequence[-1] + sequence[-2]  # Calculate the next number in the sequence
         sequence.append(next_number)  # Add the next number to the sequence
-    return sequence
+    return sequence[-1]
 
-print(fibonacci(15))
+print(fibonacci(10))
