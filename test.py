4bddeb629b74f3e8c3658fd909bc13fbf1231c65
Naty-J <natalie.jaresova@student.gyarab.cz>
2024-11-21 10:50:43 +0100

    fibonacci commit

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 def fibonacci(n):
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    
     sequence = [0, 1]  # Initialize the sequence with the first two numbers
     while len(sequence) < n:
         next_number = sequence[-1] + sequence[-2]  # Calculate the next number in the sequence
         sequence.append(next_number)  # Add the next number to the sequence
     return sequence
 
-print (fibonacci(5))
+print(fibonacci(15))
