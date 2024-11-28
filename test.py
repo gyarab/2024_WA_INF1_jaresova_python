ed5f91c8dfb229a09b9fc9970c49cbe857ff7e7e
Naty-J <natalie.jaresova@student.gyarab.cz>
2024-11-28 11:50:55 +0100

    gd

--- test.py
+++ test.py
@@ -83,9 +83,8 @@ def class_and_break_time(start_class, end_class):
     break_durations = [0, 5, 10, 20, 10, 10, 5, 5, 10, 10, 5, 5]
 
     total_class_time = (end_class - start_class + 1) * class_duration
-    total_break_time = sum(break_durations[start_class:end_class])
+    total_break_time = sum(break_durations[start_class:end_class]) + break_durations[end_class]
 
     return total_class_time, total_break_time
-
 
 
