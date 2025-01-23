0d875421ef6535e2501bcc3f35800d025922dd57
Naty-J <natalie.jaresova@student.gyarab.cz>
2025-01-23 11:55:17 +0100

    mlem

--- content/models.py
+++ content/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    perex = models.TextField(max_length=200)
+    text = models.TextField()
+    published = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
--- content/views.py
+++ content/views.py
@@ -2,18 +2,15 @@ from http.client import HTTPResponse
 import json
 from django.shortcuts import render
 from django.urls import reverse
+from content.models import Article
 
 # Create your views here.
 def articles(request):
-    with open('articles.json', encoding="utf-8") as file:
-        data =json.load(file)
+    articles=Article.objects.all()
 
-        return render(request, 'content/articles.html', {'data': data})
-
-        
+    return render(request, 'content/articles.html', {'articles': articles})
     
 def article(request, id):
-    with open("articles.json", encoding="utf-8") as file:
-        data = json.load(file)
-        article = data[id]
-        return render(request, 'content/article.html', {'article': article})
+    article=Article.objects.get(id=id)
+
+    return render(request, 'content/article.html', {'article': article})
