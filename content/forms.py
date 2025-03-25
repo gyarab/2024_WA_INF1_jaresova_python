b4812df49b0b5e03ce9afe05788af08987039ad2
Naty-J <natalie.jaresova@student.gyarab.cz>
2025-03-25 21:38:28 +0100

    teeny tiny hw fix

--- content/forms.py
+++ content/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class CommentForm(forms.Form):
-    name = forms.CharField(label="Vase jmeno ",max_length=100)
-    text = forms.CharField(label="Komentar ",widget=forms.Textarea)
+    name = forms.CharField(label="Your name ",max_length=100)
+    text = forms.CharField(label="Comment. ",widget=forms.Textarea)
